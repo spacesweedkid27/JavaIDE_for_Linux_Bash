6e0519268b31c4a5d926f7b862d42fa5a516f5e0
Henri Heyden <henri@heyden5.de>
2021-10-25 18:44:24 +0200

    early renderer class looking smooth...

---
+++ src/lib/ShellRenderer.java
@@ -0,0 +1,45 @@
+package lib;
+
+public class ShellRenderer extends Thread {
+
+    public ShellRenderer(){
+
+    }
+    public ShellRenderer(long milliseconds){
+        this.milliseconds = milliseconds;
+    }
+
+
+    private long milliseconds;
+    private String frame;
+
+    public void setMilliseconds(long milliseconds){
+        this.milliseconds = milliseconds;
+    }
+
+    public String getFrame(){
+        return this.frame;
+    }
+
+    public void setFrame(String frame){
+        this.frame = frame;
+    }
+
+    @Override
+    public void run() {
+        while (true){
+        try {
+        sleep(milliseconds);}
+        catch (InterruptedException interruptedException){
+            interruptedException.printStackTrace();
+        }
+
+            System.out.print(frame);
+
+        }
+
+    }
+
+
+
+}
