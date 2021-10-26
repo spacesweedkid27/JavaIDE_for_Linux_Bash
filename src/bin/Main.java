4719e2b2d54e85877c8532835feb0155e19219e0
Henri Heyden <henri@heyden5.de>
2021-10-26 19:39:39 +0200

    render test, everything nominal (please use a linux shell to execute this)

--- src/bin/Main.java
+++ src/bin/Main.java
@@ -3,47 +3,44 @@ package bin;
 import lib.ShellAccessor;
 import lib.ShellRenderer;
 
-import java.util.Locale;
-
 
 public class Main extends ShellAccessor {
     static String test =
             """
-                    private static String getThisShite(String input){
-                            StringBuilder temp = new StringBuilder(input.length());
-                            String miniTemp = "";
-                            for(int i = 0; i < input.length(); i++){
-                                if (input.toCharArray()[i] == '\\n'){
-                                    temp.append(miniTemp + ", ");
-                                    miniTemp = "";
-                                } else if (input.toCharArray()[i] == '0'){
-                                    temp.append(miniTemp);
-                                    return temp.toString();
-                                } else
-                                    miniTemp = miniTemp + input.toCharArray()[i];
-                            }
-                            return null;
+                    private static String getThisShite(String input){                 
+                            StringBuilder temp = new StringBuilder(input.length());   
+                            String miniTemp = "";                                     
+                            for(int i = 0; i < input.length(); i++){                  
+                                if (input.toCharArray()[i] == '\\n'){                 
+                                    temp.append(miniTemp + ", ");                     
+                                    miniTemp = "";                                    
+                                } else if (input.toCharArray()[i] == '0'){            
+                                    temp.append(miniTemp);                            
+                                    return temp.toString();                           
+                                } else                                                
+                                    miniTemp = miniTemp + input.toCharArray()[i];     
+                            }                                                         
+                            return null;                                              
                         }""";
 
 
+    public static void main(String[] args) throws InterruptedException {
 
-
-    public static void main(String[]args) throws InterruptedException {
-
-        long milliseconds = 20;
+        long milliseconds = 25;
 
         ShellRenderer shellRenderer = new ShellRenderer(milliseconds);
         shellRenderer.start();
 
-        while (true){
-            Thread.sleep(milliseconds);
-            shellRenderer.setFrame(test.toUpperCase());
-            Thread.sleep(milliseconds);
-            shellRenderer.setFrame(test.toLowerCase());
+        while (true) {
+            for (int i = 0; i < test.length(); i++) {
+                Thread.sleep(milliseconds);
+                shellRenderer.setFrame(test.substring(0, i) + "<*>" + test.substring(i + 2));
+            }
+
+
         }
 
     }
-
 
 
 }
--- src/lib/ShellRenderer.java
+++ src/lib/ShellRenderer.java
@@ -2,10 +2,11 @@ package lib;
 
 public class ShellRenderer extends Thread {
 
-    public ShellRenderer(){
+    public ShellRenderer() {
 
     }
-    public ShellRenderer(long milliseconds){
+
+    public ShellRenderer(long milliseconds) {
         this.milliseconds = milliseconds;
     }
 
@@ -13,33 +14,35 @@ public class ShellRenderer extends Thread {
     private long milliseconds;
     private String frame;
 
-    public void setMilliseconds(long milliseconds){
+    public void setMilliseconds(long milliseconds) {
         this.milliseconds = milliseconds;
     }
 
-    public String getFrame(){
+    public String getFrame() {
         return this.frame;
     }
 
-    public void setFrame(String frame){
+    public void setFrame(String frame) {
         this.frame = frame;
     }
 
     @Override
     public void run() {
-        while (true){
-        try {
-        sleep(milliseconds);}
-        catch (InterruptedException interruptedException){
-            interruptedException.printStackTrace();
-        }
 
+
+        while (true) {
             System.out.print(frame);
+            try {
+                sleep(milliseconds);
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
 
         }
 
     }
-
 
 
 }
