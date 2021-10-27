c5a9650f21ff285c661f9cd1d37beeb6f5bdcea2
Henri Heyden <henri@heyden5.de>
2021-10-27 15:15:47 +0200

    let's see if this works

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@ mkdir java
 tar -C ./java -xzvf ./jdk-17_linux-aarch64_bin.tar.gz
 rm jdk-17_linux-aarch64_bin.tar.gz
 echo "jdk 17 installed in ./java"
-./java/bin/javac -d ./bin ./src/lib/*.java
-./java/bin/javac -d ./bin -cp ./bin ./src/bin/*.java
+./java/jdk-17.0.1/bin/javac -d ./bin ./src/lib/*.java
+./java/jdk-17.0.1/bin/javac -d ./bin -cp ./bin ./src/bin/*.java
 echo "installation complete"
