942228920a6d3c2ec49aaf9461ef7d8862909e61
Henri Heyden <henri@heyden5.de>
2021-10-27 18:27:47 +0200

    Added some sexy JNativeHooks

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ tar -C ./java -xzvf ./jdk-17_linux-x64_bin.tar.gz
 rm jdk-17_linux-x64_bin.tar.gz
 echo "jdk 17 installed in ./java"
 ./java/jdk-17.0.1/bin/javac -d ./bin ./src/lib/*.java
-./java/jdk-17.0.1/bin/javac -d ./bin -cp ./bin ./src/bin/*.java
+./java/jdk-17.0.1/bin/javac -d ./bin -cp .:/bin:./ext/jnativehook-2.2.0.jar ./src/bin/*.java
 chmod +x ./uninstall.sh
 chmod +x "./bin/LinuxBash\ Java\ IDE.sh"
 echo "installation complete"
--- src/bin/Main.java
+++ src/bin/Main.java
@@ -1,5 +1,9 @@
 package bin;
 
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 import lib.ShellAccessor;
 import lib.ShellRenderer;
 
@@ -40,8 +44,31 @@ public class Main extends ShellAccessor {
     }
 
     public static void main(String[] args) {
-        ShellRenderer shellRenderer = new ShellRenderer(200);
-        shellRenderer.start();
+        //ShellRenderer shellRenderer = new ShellRenderer(200);
+        //shellRenderer.start();
+
+        NativeKeyListener nativeKeyListener = new NativeKeyListener() {
+            @Override
+            public void nativeKeyPressed(NativeKeyEvent nativeEvent) {
+                System.out.println(NativeKeyEvent.getKeyText(nativeEvent.getKeyCode()));
+            }
+        };
+
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
+
+        GlobalScreen.addNativeKeyListener(nativeKeyListener);
+
+
+        while (true) {
+
+        }
 
     }
 
