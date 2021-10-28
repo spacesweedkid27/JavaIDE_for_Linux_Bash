eda8ce3b1d8417b814a35db2250cd4f84901ecd6
Henri Heyden <henri@heyden5.de>
2021-10-28 21:24:52 +0200

    smool error

--- JavaDEV.Windows/compile.bat
+++ JavaDEV.Windows/compile.bat
@@ -1,2 +1,3 @@
+cd ..
 ..\..\Apps\jdk-17.0.1\bin\javac -cp .\bin\jnativehook-2.2.0.jar -d .\bin .\src\lib\*.java
 ..\..\Apps\jdk-17.0.1\bin\javac -cp ".\bin\;.\bin\jnativehook-2.2.0.jar" -d .\bin .\src\bin\Main.java
--- JavaDEV.Windows/run.bat
+++ JavaDEV.Windows/run.bat
@@ -1,1 +1,2 @@
+cd ..\bin
 ..\..\..\Apps\jdk-17.0.1\bin\java -cp ";.\jnativehook-2.2.0.jar" bin.Main
