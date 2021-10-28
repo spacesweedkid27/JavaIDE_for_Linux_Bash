7e104c9b5863ba2df2137069ae15cf9b125dd063
Henri Heyden <henri@heyden5.de>
2021-10-28 21:46:45 +0200

    made it so that when you run this shite on Window 10 it doesn't try to color the text.

--- JavaDEV.Windows/compile.bat
+++ JavaDEV.Windows/compile.bat
@@ -1,3 +1,4 @@
+echo "you will need the jdk-17.0.1 one directory above the repository"
 cd ..
-..\..\Apps\jdk-17.0.1\bin\javac -cp .\bin\jnativehook-2.2.0.jar -d .\bin .\src\lib\*.java
-..\..\Apps\jdk-17.0.1\bin\javac -cp ".\bin\;.\bin\jnativehook-2.2.0.jar" -d .\bin .\src\bin\Main.java
+..\jdk-17.0.1\bin\javac -cp .\bin\jnativehook-2.2.0.jar -d .\bin .\src\lib\*.java
+..\jdk-17.0.1\bin\javac -cp ".\bin\;.\bin\jnativehook-2.2.0.jar" -d .\bin .\src\bin\Main.java
