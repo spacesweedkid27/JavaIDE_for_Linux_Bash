9cc9e3fae7002167179868a57dc8bbadcfa6690d
spacesweedkid27 <57177901+spacesweedkid27@users.noreply.github.com>
2021-11-03 12:42:34 +0100

    it now works

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 echo "jdk 17 must be in ../"
-../jdk-17.0.1/bin/javac -d ./bin ./src/lib/*.java
-../jdk-17.0.1/bin/javac -d ./bin -cp ./bin/:./bin/jnativehook-2.2.0.jar ./src/bin/*.java
+../jdk-17.0.1/bin/javac -d ./bin -cp "./bin/jnativehook-2.2.0.jar" ./src/lib/*.java
+../jdk-17.0.1/bin/javac -d ./bin -cp "./bin/:./bin/jnativehook-2.2.0.jar" ./src/bin/*.java
 chmod +x "./uninstall.sh"
-chmod +x "./bin/'LinuxBash\ Java\ IDE.sh'"
+chmod +x ./bin/LinuxBash\ Java\ IDE.sh
 echo "installation complete"
